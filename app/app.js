44c7a19ed069daccee9e63d4cda25238695b6ca0
choihb <chb0127@naver.com>
2022-06-29 22:37:14 +0900

    유튜브 강좌 12

---
+++ app/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/*
+const http=require('http');
+const app=http.createServer((req,res)=>{
+    // console.log(req.url); // req.url = 요청받은 경로
+    res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+    if(req.url=='/'){
+        res.end('here is http root');
+    }
+    else if (req.url=='/login'){
+        res.end('here is http login');
+    }
+});
+
+app.listen(3001,()=>{
+    console.log('http server on. port=3001');
+});
+*/ //위에는 http로 서버열기 맛보기
+
+//모듈
+const express=require('express');
+const app=express();
+
+//라우팅
+const home=require('./src/routes/home')
+
+//앱 셋팅
+app.set('views','./src/views');
+app.set('view engine','ejs');
+
+app.use('/',home); // use 는 미들웨어를 등록해주는 메소드
+
+module.exports=app;
+
