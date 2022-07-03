b9ca9f8e823a78fb77ad6f76984b98b5d3a2bfd4
choihb <chb0127@naver.com>
2022-07-04 03:04:14 +0900

    유튜브 강좌 19

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,5 +23,16 @@ function login(){
     })
     .then((res)=>res.json()) // 백엔드에서 받은 res(promis)
     //.then((res)=> console.log(res)); 아래와 동일. 파라미터로 받은값이 다시 함수의 파라미터로 들어가는 경우 생략가능
-    .then(console.log);
+    //.then(console.log);
+    .then((res)=>{
+        if (res.success){ // 백엔드에서 받은 res의 success값이 true일 때 = 로그인 성공일 때
+            location.href='/'; // 루트경로로 이동
+        }else{
+            alert(res.msg); // false 경우 msg alert로 출력
+            //alert은 크롬 중앙상단에 뜨는 메시지
+        }
+    })
+    .catch((err)=>{ // 에러발생시 구문
+        console.error(new Error('로그인중 에러발생')); // 콘솔에 로그인중 에러발생 출력
+    });
 }
