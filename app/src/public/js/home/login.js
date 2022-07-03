16506d8ff0b5b352c9c9567508b7224b7051747a
choihb <chb0127@naver.com>
2022-07-04 00:25:50 +0900

    유튜브 강좌 15

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
-"use strict"
+"use strict";
 
-console.log('hello');
+//DOM= 문서객체모델. 인터페이스. html에 데이터를 받아옴
+//document 라이브러리 이용
+const id=document.querySelector('#id'), // html id 선택자 id를 받아옴
+    psword=document.querySelector('#psword'), //html id선택자 psword 받아옴
+    loginBtn=document.querySelector('button');
+
+loginBtn.addEventListener('click',login); // loginBtn이 click 이벤트 발생시 login 실행
+
+function login(){
+    const req={
+        id:id.value, // 선택자 id 의 값을 받아옴
+        psword:id.value // 선택자 psword 의 값을 받아옴
+    };
+    console.log(req);
+}
