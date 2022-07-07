5d0ac0f11ac71adea608c5ca5211fa059e02f065
choihb <chb0127@naver.com>
2022-07-07 22:22:24 +0900

    유튜브 강좌 23

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,6 +9,7 @@ const ctrl=require('./home.ctrl');
 
 router.get('/',ctrl.output.home);
 router.get('/login',ctrl.output.login);
+router.get('/register',ctrl.output.register);
 router.post('/login',ctrl.process.login); // 프론트엔드가 전달한 login
 
 module.exports=router;
