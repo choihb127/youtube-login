06d4781f9d357d3e2cdb1ec01142b3a1018277fb
choihb <chb0127@naver.com>
2022-07-11 04:42:02 +0900

    유튜브 강좌 26(작동X)

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,16 +2,12 @@
 //MVC 모델중 M에 해당됨
 'use strict';
 
+const fs=require('fs').promises;
+
 class UserStorange{
-    static #users={ // 임시로 생성된 ID와 PW 리스트 => 등록된 계정
-        // 클래스 변수에 #추가로 private 선언
-        // static 선언으로 인스턴스(require) 없이 사용가능
-        good_id:['id1','id2','id3'],
-        good_psword:['1111','1111','1234'],
-        good_name:['name1','name2','name3']
-    };
-
-
+ // 임시로 생성된 ID와 PW 리스트 => 등록된 계정
+    // 클래스 변수에 #추가로 private 선언
+    // static 선언으로 인스턴스(require) 없이 사용가능
     static getUsers(...fields){ // ... 형태 의미?
         const userinfo=this.#users;
         // 이하 reduce 이해필요. 반복문?
@@ -26,16 +22,19 @@ class UserStorange{
     };
 
     static getUserInfo(id){
-        const users=this.#users;
+       // const users=this.#users; // database 생성으로 이용하지 않음
+       fs.readFile('./src/database/users.json',(err,data)=>{
+        if(err){throw err;}
+        const users=JSON.parse(data);
         const idx=users.good_id.indexOf(id); // 매개변수로 받은 id 와 일치하는 인덱스
         const userKeys=Object.keys(users); // users의 key값만 가져와 배열 생성 => {id,psword,name} 형태
         const userinfo=userKeys.reduce((newUser,info)=>{
             newUser[info]=users[info][idx];
             return newUser;
         },{});
-
         return userinfo;
-    };
+    });
+    }
 
     static save(userinfo){
         const user=this.#users;
